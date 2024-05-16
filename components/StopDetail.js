9161756f8a477c57638d5e0dd9aad15f4a0d7c77
Lara Simonetti <lara.simonetti@ssseco.ch>
2024-05-16 10:20:34 +0200

    stopdetail

---
+++ components/StopDetail.js
@@ -0,0 +1,8 @@
+/*
+    StopDetail.js
+    Autore: Lara Simonetti
+    Utilità: questa componente è stata sviluppata per ...
+    Utilizzo: per utilizzare questa componente è necessario includerla ...., 
+passarle in argomento (props, ...) n dati nel formato yyyy
+*/
+
